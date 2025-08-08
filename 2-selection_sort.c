6467a2d866040002f98fe12ea15e7e3e7451814d
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-08 15:34:07 +0200

    creation des mains manquant + modif 1-O

---
+++ 2-selection_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order using the
+ *                  Selection sort algorithm.
+ *
+ * @array: A pointer to the array of integers to be sorted.
+ * @size: The size of the array.
+ */
+
+void selection_sort(int *array, size_t size)
+{
+
+}
